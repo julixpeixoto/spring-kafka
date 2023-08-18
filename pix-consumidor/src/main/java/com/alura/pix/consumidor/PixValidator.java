b3df88c14e0ad1a820bdfcf067a4d80adb390960
Julierme Peixoto <julierme2005@hotmail.com>
2023-08-17 21:40:39 -0300

    Add retry topics

--- pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
+++ pix-consumidor/src/main/java/com/alura/pix/consumidor/PixValidator.java
@@ -2,13 +2,16 @@ package com.alura.pix.consumidor;
 
 import com.alura.pix.dto.PixDTO;
 import com.alura.pix.dto.PixStatus;
+import com.alura.pix.exception.KeyNotFoundException;
 import com.alura.pix.model.Key;
 import com.alura.pix.model.Pix;
 import com.alura.pix.repository.KeyRepository;
 import com.alura.pix.repository.PixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +24,12 @@ public class PixValidator {
     private PixRepository pixRepository;
 
     @KafkaListener(topics = "pix-topic", groupId = "grupo")
+    @RetryableTopic (
+            backoff = @Backoff(value = 3000), // tempo de espera para tentar novamente em ms
+            attempts = "5", // número de retentativas
+            autoCreateTopics = "true", // habilita criação de novos tópicos, ex. pix-topic-1, pix-topic-2, etc
+            include = KeyNotFoundException.class // exceção considerada na retentativa
+    )
     public void processaPix(PixDTO pixDTO, Acknowledgment acknowledgment) {
         // notifica manualmente o processamento da mensagem
         acknowledgment.acknowledge();
@@ -34,6 +43,7 @@ public class PixValidator {
 
         if (origem == null || destino == null) {
             pix.setStatus(PixStatus.ERRO);
+            throw new KeyNotFoundException();
         } else {
             pix.setStatus(PixStatus.PROCESSADO);
         }
